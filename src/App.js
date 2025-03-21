cdce7d1fb3a491292236c63412a8e569f17ca524
Aya <ayadaaboul02@outlook.com>
2025-03-21 23:24:08 +0200

    css and webvitals

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
 // import Header from "./components/Header";
 
 function App() {
