3449e0ce502b1a60e94387c7a09157dc16ba537c
zeinab <zeinabhteit1@gmail.com>
2025-03-28 14:06:43 +0200

    updated ProductList.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
-// import DashboardPage from './pages/DashboardPage';
+import Login from './components/login';
+// import Dashboard from './pages/dashboard';
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        {/* <ProtectedRoute path="/dashboard" element={<DashboardPage />} /> */}
+
         {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
       </Routes>
     </Router>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getAllProducts } from "../services/apiService";
+import ProductCard from "./ProductCard";
 
 const ProductsList = () => {
   const [products, setProducts] = useState([]);
@@ -17,7 +18,6 @@ const ProductsList = () => {
         setLoading(false);
       }
     };
-
     fetchProducts();
   }, []);
 
@@ -28,24 +28,8 @@ const ProductsList = () => {
     <div className="container mx-auto p-6">
       {/* Product Grid Layout */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {products.map((product) => (
-          <div key={product.id} className="bg-white p-4 rounded-lg shadow-md hover:shadow-2xl transition" >
-            {/* Product Image */}
-            {product.images && product.images.length > 0 && (
-              <img
-                src={product.images[0]} // Fixed: Get the first image from array
-                alt={product.name}
-                className="w-full h-48 object-contain rounded-md"
-              />
-            )}
-
-            {/* Product Details */}
-            <div className="mt-4">
-              <h3 className="text-lg font-semibold">{product.name}</h3>
-              <p className="text-gray-600 text-sm">{product.description}</p>
-              <p className="text-lg font-bold text-blue-600 mt-2">{product.price} $</p>
-            </div>
-          </div>
+        {products.map((product, index) => (
+          <ProductCard key={product.id} product={product} index={index} />
         ))}
       </div>
     </div>
@@ -53,4 +37,6 @@ const ProductsList = () => {
 };
 
 export default ProductsList;
+
+
 
