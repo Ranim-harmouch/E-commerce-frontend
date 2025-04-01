e15fed2ec2164d0e2acfa5af7f86f828e66da43b
ranim <ranim17har@gmail.com>
2025-04-02 02:30:11 +0300

    edit

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,26 +1,3 @@
-// import React from "react";
-
-// const ProductImages = ({ images }) => {
-//   return (
-//     <div>
-//       <img src={images[0]} alt="Product" className="w-full mb-4" />
-//       <div className="flex space-x-2">
-//         {images.map((image, index) => (
-//           <img
-//             key={index}
-//             src={image}
-//             alt={`Thumbnail ${index + 1}`}
-//             className="w-20 cursor-pointer"
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ProductImages;
-
-
 
 import React from "react";
 
